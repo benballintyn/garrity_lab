f8ce634451e152661701df9c3249ad6474b71a8c
benballintyn <bbal@brandeis.edu>
2022-05-26 17:46:41 -0400

    added warnings and a synchErrorTolerance

--- garrityVidTempSynch.m
+++ garrityVidTempSynch.m
@@ -3,6 +3,8 @@ function [synchedData] = garrityVidTempSynch(vidDataFname,tempDataFname,varargin
 
 % Disable warnings until end of table creation
 warning('off','all')
+nWarns = 0; % Counts number of warnings to produce at the end
+warnFlags = {};
 
 % Parse inputs
 isASynchMethod = @(x) ismember(x,{'exactInterp','nearestNeighbor'});
@@ -10,6 +12,7 @@ p = inputParser;
 addRequired(p,'vidDataFname')
 addRequired(p,'tempDataFname')
 addParameter(p,'synchMethod','exactInterp',isASynchMethod)
+addParameter(p,'synchErrorTolerance',100,@isnumeric)
 parse(p,vidDataFname,tempDataFname,varargin{:})
 
 % Read in data. Temp data is read into a table. Video metadata is read as a
@@ -50,12 +53,12 @@ vidMsecs = str2num(vidTimeStamp(dotInds(1)+1:spaceInds(2)-1));
 vidDayTimeInMsecs = (vidHrs*3600 + vidMins*60 + vidSecs)*1000 + vidMsecs;
 
 % Convert temp table into new table with times in ms and temperatures in
-% celcius (conversion from voltage)
-tempOnlyTable = cell2table(cell(0,2),'VariableNames',{'Timestamp','Celcius'});
+% celsius (conversion from voltage)
+tempOnlyTable = cell2table(cell(0,2),'VariableNames',{'Timestamp','Celsius'});
 startInd = 5;
 for i=startInd:height(T)
     tempOnlyTable.Timestamp(i-startInd+1) = tempDayTimeInMsecs + str2num(T.Var1{i})*1000;
-    tempOnlyTable.Celcius(i-startInd+1) = str2num(T.Var2{i})*0.04113 - 0.67726;
+    tempOnlyTable.Celsius(i-startInd+1) = str2num(T.Var2{i})*0.04113 - 0.67726;
 end
 
 % Extract number of video frames
@@ -101,25 +104,37 @@ else
 end
 
 % Synch data using desired method
-synchedData = cell2table(cell(0,7),'VariableNames',{'Frame','TimeElapsed','Celcius','SynchError','VideoTime','TempTime','isInterpolated'});
+synchedData = cell2table(cell(0,7),'VariableNames',{'Frame','TimeElapsed','Celsius','SynchError','VideoTime','TempTime','isInterpolated'});
 for i=1:frameCount
     synchedData.Frame(i) = vidOnlyTable.frame(i);
     synchedData.TimeElapsed(i) = vidOnlyTable.elapsed_time(i);
     if (strcmp(p.Results.synchMethod,'exactInterp'))
         tdiffs = vidOnlyTable.Timestamp(i) - tempOnlyTable.Timestamp;
         [~,bestInd] = min(abs(tdiffs));
+        if (abs(tdiffs(bestInd)) > p.Results.synchErrorTolerance)
+            if (~ismember(warnFlags,'synchToleranceExceeded'))
+                nWarns = nWarns+1;
+                warnFlags{nWarns} = 'synchToleranceExceeded';
+            end
+            synchedData.Celsius(i) = nan;
+            synchedData.SynchError(i) = nan;
+            synchedData.VideoTime(i) = vidOnlyTable.Timestamp(i);
+            synchedData.TempTime(i) = nan;
+            synchedData.isInterpolated(i) = false;
+            continue;
+        end
         if (tdiffs(bestInd) > 0)
             if (bestInd ~= height(tempOnlyTable))
                 diff1 = tdiffs(bestInd);
                 diff2 = tempOnlyTable.Timestamp(bestInd+1) - vidOnlyTable.Timestamp(i);
-                interpolatedTemperature = ((1/diff1)*tempOnlyTable.Celcius(bestInd) + (1/diff2)*tempOnlyTable.Celcius(bestInd+1))/((1/diff1)+(1/diff2));
-                synchedData.Celcius(i) = interpolatedTemperature;
+                interpolatedTemperature = ((1/diff1)*tempOnlyTable.Celsius(bestInd) + (1/diff2)*tempOnlyTable.Celsius(bestInd+1))/((1/diff1)+(1/diff2));
+                synchedData.Celsius(i) = interpolatedTemperature;
                 synchedData.SynchError(i) = abs(tdiffs(bestInd));
                 synchedData.VideoTime(i) = vidOnlyTable.Timestamp(i);
                 synchedData.TempTime(i) = tempOnlyTable.Timestamp(i);
                 synchedData.isInterpolated(i) = true;
             else
-                synchedData.Celcius(i) = tempOnlyTable.Celcius(bestInd);
+                synchedData.Celsius(i) = tempOnlyTable.Celsius(bestInd);
                 synchedData.SynchError(i) = abs(tdiffs(bestInd));
                 synchedData.VideoTime(i) = vidOnlyTable.Timestamp(i);
                 synchedData.TempTime(i) = tempOnlyTable.Timestamp(i);
@@ -129,27 +144,37 @@ for i=1:frameCount
             if (bestInd > 1)
                 diff1 = -tdiffs(bestInd);
                 diff2 = vidOnlyTable.Timestamp(i) - tempOnlyTable.Timestamp(bestInd-1);
-                interpolatedTemperature = ((1/diff1)*tempOnlyTable.Celcius(bestInd) + (1/diff2)*tempOnlyTable.Celcius(bestInd-1))/((1/diff1)+(1/diff2));
-                synchedData.Celcius(i) = interpolatedTemperature;
+                interpolatedTemperature = ((1/diff1)*tempOnlyTable.Celsius(bestInd) + (1/diff2)*tempOnlyTable.Celsius(bestInd-1))/((1/diff1)+(1/diff2));
+                synchedData.Celsius(i) = interpolatedTemperature;
                 synchedData.SynchError(i) = abs(tdiffs(bestInd));
                 synchedData.VideoTime(i) = vidOnlyTable.Timestamp(i);
                 synchedData.TempTime(i) = tempOnlyTable.Timestamp(i);
                 synchedData.isInterpolated(i) = true;
             else
-                synchedData.Celcius(i) = tempOnlyTable.Celcius(bestInd);
+                synchedData.Celsius(i) = tempOnlyTable.Celsius(bestInd);
                 synchedData.SynchError(i) = abs(tdiffs(bestInd));
                 synchedData.VideoTime(i) = vidOnlyTable.Timestamp(i);
                 synchedData.TempTime(i) = tempOnlyTable.Timestamp(i);
                 synchedData.isInterpolated(i) = false;
             end
         else
-            synchedData.Celcius(i) = tempOnlyTable.Celcius(bestInd);
+            synchedData.Celsius(i) = tempOnlyTable.Celsius(bestInd);
             synchedData.SynchError(i) = 0;
             synchedData.VideoTime(i) = vidOnlyTable.Timestamp(i);
             synchedData.TempTime(i) = tempOnlyTable.Timestamp(i);
             synchedData.isInterpolated(i) = false;
         end
         
+    end
+end
+
+warning('on','all')
+for i=1:nWarns
+    switch warnFlags{i}
+        case 'synchToleranceExceeded'
+            warning(['At least one data point exceeded the allowed synchErrorTolerance, currently set at ' ...
+                     num2str(p.Results.synchErrorTolerance) 'ms. This may be because the temperature recording' ...
+                     ' was started after the video recording.'])
     end
 end
 end
